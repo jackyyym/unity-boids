e58218d2437e65086b65f5288e55e007ac3e69b0
jackyyym <jac.smcgrath@gmail.com>
2021-02-15 13:45:44 -0500

    create boid prefab, scene setup

--- Boids/Assets/Scripts/Controller.cs
+++ Boids/Assets/Scripts/Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller2D : MonoBehaviour
+public class Controller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
